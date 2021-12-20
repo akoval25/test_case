18832d8088d56c103ebc1c3bb62a7d4c098abff0
akoval25 <88685936+akoval25@users.noreply.github.com>
2021-12-20 11:36:30 +0200

    select

--- calculator.js
+++ calculator.js
@@ -1,34 +1,43 @@
 
-const calcAction = document.querySelector('#action');
+
 const form = document.querySelector(".form__btm");
+const select = document.querySelector('.form__select');
+  
+select.addEventListener('change', function () {
+    
+  arr = {
+    'plus': '+',
+    'minus': '-',
+    'multiplication': '*',
+    'division': '/',
+   };
+    let key = this.value;
+    array = arr[key];
 
-// console.log(calcAction[0].dataset.action);
-
+  
 form.addEventListener("click", (event) => {
 
-event.preventDefault();
+  event.preventDefault();
+
+  
   const firstNum = document.querySelector('.js-input__first');
   const secondNum = document.querySelector('.js-input__second');
   const result = document.querySelector('.js-result');
 
-  switch (calcAction[0].dataset.action) {
-  // const formData = new FormData(event.currentTarget);
-  // console.log(formData); 
-  // switch (event.currentTarget.elements.action.value) {
-  case "plus":
-    // return form.insertAdjacentHTML("afterend", `<p>Результат: ${Math.round(+firstNum.value + +secondNum.value)}</p>`);
-    return result.innerHTML = `<p class="js-result">Результат: ${Math.round(+firstNum.value + +secondNum.value)}</p>`;
-  case "minus":
-    return result.innerHTML = `<p class="js-result">Результат: ${Math.round(+firstNum.value - +secondNum.value)}</p>`;
-  case "multiplication":
-    return result.innerHTML = `<p class="js-result">Результат: ${Math.round(+firstNum.value * +secondNum.value)}</p>`;
-  case "division":
-    return result.innerHTML = `<p class="js-result">Результат: ${Math.round(+firstNum.value / +secondNum.value)}</p>`;
+  switch (array) {
+  case "+":
+    return result.innerHTML = `<p class="js-result">Результат: ${Math.floor(+firstNum.value + +secondNum.value)}</p>`;
+  case "-":
+    return result.innerHTML = `<p class="js-result">Результат: ${Math.floor(+firstNum.value - +secondNum.value)}</p>`;
+  case "*":
+    return result.innerHTML = `<p class="js-result">Результат: ${Math.floor(+firstNum.value * +secondNum.value)}</p>`;
+  case "/":
+    return result.innerHTML = `<p class="js-result">Результат: ${Math.floor(+firstNum.value / +secondNum.value)}</p>`;
   default:
     return result.innerHTML = `<p class="js-result">Щось пішло не так</p>`;
 }
-
 });
+  });
 
 
 
